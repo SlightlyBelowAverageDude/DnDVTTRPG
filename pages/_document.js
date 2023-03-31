4d3033edfe2944709c7c57360242c72739455e5b
EnderArchery <43007119+EnderArchery@users.noreply.github.com>
2023-03-31 23:13:53 +0200

    Added p5.js for rendering
    Added p5.js for rendering the grid and the images, as well as creating the drag and drop interfaces

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="de">
       <Head />
       <body>
+        <Script
+          src="p5.min.js"
+          strategy="afterInteractive"
+        />
         <Main />
         <NextScript />
       </body>
